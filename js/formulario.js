95ed3d82d9e807315d9fc358851f2a71f8cf742c
luigi-zuniga <luis.alberto.zuniga@outlook.comexit>
2017-01-16 18:52:48 -0300

    Cambio de contenido, comprobacion ej2

--- js/formulario.js
+++ js/formulario.js
@@ -1,51 +1,28 @@
 function entraEnFoco(elemento)
 {
 	elemento.className = "enFoco";
-	/*
-	  parentNode 
-	  hace referencia al elemento padre que lo contiene
-	  en este caso input es contenido por li	
-	*/
-	var elementoLista = elemento.parentNode;
-	/*Si ejecutamos el siguiente ejemplo veremos que 
-	  todos los elementos que este entre li y /li se convierten 
-	  automaticamente en parrafos que damos por innerHTML
-	  esto ocurre por el hecho de estar igualando el elemento lista
-	  por la etiqueta p con el mensaje.
-
-	  elementoLista.innerHTML = "<p>Ayuda!</p>";
-
-	  lo que debemos hacer para visualizar el parrafo es lo siguiente
-	*/
-	var mensajeAlerta = document.getElementById("mensajeAlerta");
-	if(mensajeAlerta!=undefined)
-	{
-	/*
-		si mensajeAlerta!=definido nos dara true
-		en tal caso remueve el id
-
-		!= operador de igualdad y desigualdad, nos permiten comprobar 
-		si los valores de operando son iguales o no, independientemente
-		del tipo que sean dichos operandos.
-		el valor obtenido es true cuando la igualdad o desigualdad 
-		se cumple y false en caso contrario
-
-		undefined operacion no inicializada	
-		removeChild elimina un nodo y todos sus descendientes, 
-		donde el nodo padre es el nodo que queremos eliminar
-
-		Lo que hacemos basicamente es eliminar un elemento si ya
-		esta definido
-	*/
-
-		mensajeAlerta.parentNode.removeChild(mensajeAlerta);
-	}
-
-		elementoLista.innerHTML += "<p class='mensajeAlerta' id='mensajeAlerta'>Complete este campo!</p>";
 	
 }
 
 function salidaDeFoco(elemento)
 {
 	elemento.className = "fueraFoco";
+}
+
+/*
+Funcion que comprueba si el campo en el cual 
+estamos posicionados, se encuentra vacio. 
+*/
+function revisarObligatorio(elemento)
+{
+	//si el valor de elemento se encuentra vacio
+	if(elemento.value=="")
+	{
+		//generamos la clase error desde 
+		//input.erro de CSS
+		elemento.className = "error";
+	}else{
+		//en caso contrario lo dejamos sin clase
+		elemento.className = "";
+	}
 }
