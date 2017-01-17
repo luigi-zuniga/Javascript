9567db2f2b704b9d2267d987d98ff532ac0f621d
luigi-zuniga <luis.alberto.zuniga@outlook.comexit>
2017-01-16 23:58:21 -0300

    Validacion longitud de texto

--- js/formulario.js
+++ js/formulario.js
@@ -20,10 +20,25 @@ function revisarNumerico(elemento)
 			//isNaN comprueba si el valor es is Not A Number
 			//si el dato no es un numero generamos un error
 			elemento.className = "error";
-			alert("Haz ingresado un valor no numerico");
+			alert("Haz ingresado un valor erroneo");
 
 		}else{
 			elemento.className = "";
+		}
+	}
+}
+
+function revisarLongitud(elemento, minimo)
+{
+	//Revisamos que el campo no este vacio
+	if(elemento.value!="")
+	{
+		var dato = elemento.value;
+		if(dato.length < minimo)
+		{
+			elemento.className = "error";
+		}else{
+			elemento.className="";
 		}
 	}
 }
